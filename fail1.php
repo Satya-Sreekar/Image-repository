4ba9119d9f1b50fa9926f46bb4c1ed2365c02896
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2022-12-11 09:14:29 +0530

    Incorrect password

---
+++ fail1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <Head>
+        <title>
+            Incorrect Passowrd!
+        </title>
+        <style>
+    body 
+    {
+        background-image: url('download.jpg');
+    }
+        </style>
+
+    </Head>
+    <Body>
+       <center>
+         <H1>Incorrect Passowrd!</H1>
+         <button onclick="location.href = '/index.php'";" id="myButton" class="float-left submit-button" >Try Again!</button>
+         <br>
+         <audio controls autoplay>
+            <source src="holdup.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+         </audio>
+       </center>
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <Head>
+        <title>
+            You Are Now Logged in 
+        </title>
+        <style>
+    body 
+    {
+        background-image: url('welcome.gif');
+    }
+        </style>
+
+    </Head>
+    <Body>
+    </body>
+</html>
