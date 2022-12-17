8ba8b44aaee6b1e062dd7123213fe3e49a72c5b6
LaasyaKruthiJ <120037110+LaasyaKruthiJ@users.noreply.github.com>
2022-12-17 12:46:03 +0530

    Add files via upload

---
+++ manipulate.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+
+</head>
+<body>
+
+<h1>Manipulate database:</h1>
+
+<form action="index.php" method-="POST">
+    <button type="submit" formaction="droptables.php">Drop tables</button>
+
+    <button type="submit" formaction="createtables.php">create tables</button>
+
+    <button type="submit" formaction="insertcredentials.php">insert into tables</button>
+
+    <button type="submit" formaction="display.php">display tables</button>
+
+    <button type="submit" formaction="truncate.php">truncate data</button>
+</form>
+
+</body>
+
+</html>
