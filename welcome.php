26948fcdf8a87ee7e75fcccdef7b00fb03d2e7d9
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2022-12-18 11:23:18 +0530

    Logout functionality

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 <html>
     <Head>
         <title>
-            You Are Now Logged in! 
+            You Are Now Logged in 
         </title>
         <style>
     body 
@@ -13,5 +13,16 @@
 
     </Head>
     <Body>
+        <?php session_start();
+        if (isset($_SESSION['UN'])) {
+            echo 'Welcome ' . $_SESSION['UN'] . "<br> Logged in as " . $_SESSION['Role'];
+        }
+        else{
+            header("location:index.php");
+            exit;
+        }
+     ?>
+
+    <H1><center><a href='/logout.php'>Logout</a></center><H1>
     </body>
 </html>
