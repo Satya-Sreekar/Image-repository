ef7084596f2d27272bb99b53f30b53ba6583ae88
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2023-01-12 16:27:45 +0530

    External CSS Implemented

--- index.php
+++ index.php
@@ -1,75 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-  <style>
-    body {
-      background-color: coral;
-    margin:10%;
-    font-family: 'Ubuntu', sans-serif;
-	  background-size: 100% 110%;
-    text-align:center;
-
-    }
-    h1,a{
-    margin:0; padding:0;
-    font-size:300%;
-    color: Maroon;
-    }
-    h3{
-      margin:0; padding:0;
-    }
-    .login {
-  -webkit-border-radius: 10px 10px 10px 10px;
-  border-radius: 10px 10px 10px 10px;
-  background: #fff;
-  padding: 30px;
-  width: 90%;
-  max-width: 300px;
-  position: relative;
-  padding: 30px;
-  -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-
-}
-.log{
-  display: flex;
-  align-items: center;
-  flex-direction: column; 
-  justify-content: center;
-  width: auto;
-  min-height: 100%;
-  padding: 20px;
-}
-input{
-  margin:10px;
-}
-select{
-  margin:10px 0px 10px 0px;
-}
-input[type=submit]{
-  background-color: rgb(0,0,255);
-  border-radius:15px;
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-decoration: none;
-  margin: 4px 2px;
-  cursor: pointer;
-  
-}
-input[type=submit]:hover{
-  background-color: lightblue;
-  border-radius:15px;
-  border: none;
-  text-color: rgb(64,0,32);
-  padding: 16px 32px;
-  text-decoration: none;
-  margin: 4px 2px;
-  cursor: pointer;
-  
-}
-
-</style>
+<link rel="stylesheet" href="index.css">
 <?php session_start();
 	if(isset($_SESSION['UN'])){
 		header("location:welcome.php");
