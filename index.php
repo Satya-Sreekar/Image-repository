8c979990794a9f83cbd337fe1041fcd36c0a220b
ShahazaadAhmed <110083695+ShahazaadAhmed@users.noreply.github.com>
2023-03-08 16:21:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
 		exit;
 	}
 ?>
-<h1>LOGIN</h1>
+<div class='heading'>
+  <img src="1car.png" alt=""height=5% width=5%>
+  <center>
+  <h1 class='hh'>Oil Seeds Pests and Diseases Image Repository System V<span class='v'>2.0</span></h1>
+  </center>
+  <img src="iior-logo.jpg" alt=""height=5% width=5%>
+</div>
 <div class="log">
 <div class="login">
+<h2>LOGIN</h2>
 <form action="/validation.php" method="POST">
   
   <label for="User"><h3>User Name:</h3></label>
