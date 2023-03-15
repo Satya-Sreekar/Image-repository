762a56513523c988800c1684d77c6ecfc3f88fc4
ShahazaadAhmed <110083695+ShahazaadAhmed@users.noreply.github.com>
2023-03-15 12:34:28 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,59 +1,161 @@
 <?php
 require 'DBinfo.php';
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Aprover</title>
+  <style>
+html
+{
+    background-color: cornsilk;
+}
+th
+{
+
+  padding: auto;
+  margin-right:20%;
+    font-size:60%;
+    align-content: center;
+    border: 2px solid black;
+}
+td
+{
+  padding: auto;
+    align-items: center;
+    margin-right:50px;
+    border: 2px solid black;
+}
+.heading
+{
+    border: 2px solid rgba(72,172,76,251);
+    background-color:rgba(74,174,78,253);
+    margin-top: 0%;
+    padding-top: 5px;
+    padding-right: 90px;
+    padding-bottom: 5px;
+    padding-left: 90px;
+    box-shadow: 0 10px 20px 0 rgba(0,0,0,0.15);
+}
+table
+{
+  border-spacing:2px;
+  padding:0%;
+  margin-top:30px;
+  border: 2px solid black;
+  margin-left:10px;
+  margin-right:10px;
+  align-items:center;
+}
+.z{
+  margin-right:10px;
+  margin-left:10px;
+}
+body{
+margin:0%;  
+}
+.h{
+  margin:0%;
+}
+  </style>
+  <meta charset="UTF-8">
+  <div class='heading'>
+        <center>
+            <h1 class="h">
+                Aprover
+            </h1>
+        </center>
+        </div>
+</head>
+<body>
+<script>
+function openFullscreenImage(img) {
+  // Create a new full-screen element
+  var fullscreen = document.createElement('div');
+  fullscreen.style.position = 'fixed';
+  fullscreen.style.top = 0;
+  fullscreen.style.left = 0;
+  fullscreen.style.width = '100%';
+  fullscreen.style.height = '100%';
+  fullscreen.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  fullscreen.style.zIndex = 9999;
+
+  // Create the image element and insert it into the full-screen element
+  var imgElem = document.createElement('img');
+  imgElem.src = img.src;
+  imgElem.style.maxWidth = '100%';
+  imgElem.style.maxHeight = '100%';
+  imgElem.style.margin = 'auto';
+  fullscreen.appendChild(imgElem);
+
+  // Add the full-screen element to the document body
+  document.body.appendChild(fullscreen);
+
+  // Add a click event listener to the full-screen element to remove it when clicked
+  fullscreen.addEventListener('click', function() {
+    document.body.removeChild(fullscreen);
+  });
+}
+
+</script>
+
+<?php
 // Create database connection
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  echo " ";
 
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // select all data from the table
   $stmt = $conn->query("SELECT * FROM tempdb");
   $data = $stmt->fetchAll();
-
-  // print the data in a labeled HTML table 
-  echo "<table>
+?>
+<table>
           <tr>
-            <th>CROP</th>
-            <th>MONTH</th>
-            <th>YEAR</th>
-            <th>CROP STAGE</th>
-            <th>PARTS AFFECTED</th>
-            <th>DEVICE</th>
-            <th>SEASON</th>
-            <th>STATE</th>
-            <th>PORD</th>
-            <th>AREA</th>
-            <th>BACKGROUND</th>
-            <th>IMAGEDESC</th>
-            <th>IMAGECONT</th>
-            <th>STAGE</th>
-            <th>IMAGE</th>
-          </tr>";
-  foreach ($data as $row) {
-    echo "<tr>
-            <td>{$row['CROP']}</td>
-            <td>{$row['MONTH']}</td>
-            <td>{$row['YEAR']}</td>
-            <td>{$row['CROP STAGE']}</td>
-            <td>{$row['PARTS AFFECTED']}</td>
-            <td>{$row['DEVICE']}</td>
-            <td>{$row['SEASON']}</td>
-            <td>{$row['STATE']}</td>
-            <td>{$row['PORD']}</td>
-            <td>{$row['AREA']}</td>
-            <td>{$row['BACKGROUND']}</td>
-            <td>{$row['IMAGEDESC']}</td>
-            <td>{$row['IMAGECONT']}</td>
-            <td>{$row['STAGE']}</td>
-            <td><img src=\"data:image/jpeg;base64,".base64_encode($row['IMAGE'])."\" /></td>
-          </tr>";
-  }
+            <th><h2 class="z">CROP</h2></th>
+            <th><h2 class="z">MONTH</h2></th>
+            <th><h2 class="z">YEAR</h2></th>
+            <th><h2 class="z">CROP STAGE</h2></th>
+            <th><h2 class="z">PARTS AFFECTED</h2></th>
+            <th><h2 class="z">DEVICE</h2></th>
+            <th><h2 class="z">SEASON</h2></th>
+            <th><h2 class="z">STATE</h2></th>
+            <th><h2 class="z">PORD</h2></th>
+            <th><h2 class="z">AREA</h2></th>
+            <th><h2 class="z">BACKGROUND</h2></th>
+            <th><h2 class="z">IMAGEDESC</h2></th>
+            <th><h2 class="z">IMAGECONT</h2></th>
+            <th><h2 class="z">STAGE</h2></th>
+            <th><h2 class="z">IMAGE</h2></th>
+          </tr>"<?php
+          foreach ($data as $row) {
+            echo "<tr>
+                    <td>{$row['CROP']}</td>
+                    <td>{$row['MONTH']}</td>
+                    <td>{$row['YEAR']}</td>
+                    <td>{$row['CROP STAGE']}</td>
+                    <td>{$row['PARTS AFFECTED']}</td>
+                    <td>{$row['DEVICE']}</td>
+                    <td>{$row['SEASON']}</td>
+                    <td>{$row['STATE']}</td>
+                    <td>{$row['PORD']}</td>
+                    <td>{$row['AREA']}</td>
+                    <td>{$row['BACKGROUND']}</td>
+                    <td>{$row['IMAGEDESC']}</td>
+                    <td>{$row['IMAGECONT']}</td>
+                    <td>{$row['STAGE']}</td>
+                    <td><img src=\"data:image/jpeg;base64,".base64_encode($row['IMAGE'])."\" onclick=\"openFullscreenImage(this)\" style=\"width: 200px; height: 200px; cursor: pointer;\" /></td>
+                  </tr>";
+          }          
   echo "</table>";
 
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+?>
+</body>
+</html>
