3af23fd9392a81a5201a702cc018f1f4b8b4573c
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2023-03-20 19:39:29 +0530

    Bug fixed

--- manipulate.php
+++ manipulate.php
@@ -12,35 +12,6 @@ if (!isset($_SESSION['UN']))
 require('DBinfo.php');
 
 ?>
-</head>
-<body>
-
-<h1>ADMIN</h1>
-
-<form>
-    <button type="submit" formaction="addusers.php">ADD USERS</button>
-    <BR><BR>
-    <button type="submit" formaction="manipulatedata.php">MANIPULATE CROP DATA</button>
-    <BR><BR>
-    <button type="submit" formaction="insertcredentials.php">VIEW APPROVED ENTRIES</button>
-</form>
-
-</body>
-
-</html><!DOCTYPE HTML>
-<html>
-<head>
-<?php
-session_start ();
-if (!isset($_SESSION['UN']))
-{
-    header("location:index.php");
-    exit;
-}
-// Connect to the database
-require('DBinfo.php');
-
-?>
 <style>
 body{
     margin:0%;
