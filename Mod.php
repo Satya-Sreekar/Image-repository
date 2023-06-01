e8c25410cc6c89a80e785c23a06cb6a20353ce06
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2023-06-01 20:15:11 +0530

    Deleted Obselete files

--- Mod.php
+++
@@ -1,4 +0,0 @@
-<?php echo "Aproover"?>
-<html>
-<H1><center><a href='/logout.php'>Logout</a></center><H1>
-</html>
--- final.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!isset($_SESSION['UN']))
-{
-    header("location:index.php");
-    exit;
-}
-
-?>
