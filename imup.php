b0802bb11a268e1b6a07cca71efdbfdfdc1870cd
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2023-06-01 20:00:04 +0530

    Made the code more redable and more efficient. changes to acomodate Nutrient Deficiency

--- imup.php
+++ imup.php
@@ -1,59 +1,63 @@
+<?php
+session_start();
+require('DBinfo.php');
+
+// Redirect to index.php if user is not logged in
+if (!isset($_SESSION['UN'])) {
+    header("location: index.php");
+    exit;
+}
+
+// Database connection
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $uploads_dir = 'images/';
+
+    if (empty($_FILES['images']['name'][0])) {
+        echo "<script>alert('Please select at least one image to upload.');</script>";
+    } else {
+        // Perform image upload process here
+    }
+}
+?>
+
 <html>
 
 <head>
-    <?php
-    session_start();
-    require('DBinfo.php');
-    if (!isset($_SESSION['UN'])) {
-        header("location:index.php");
-        exit;
-    }
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $uploads_dir = 'images/'; // set the directory path where images will be uploaded
-
-        if (empty($_FILES['images']['name'][0])) { // check if no image files have been uploaded
-            echo "<script>alert('Please select at least one image to upload.');</script>"; // display an alert message
-        } else { // proceed with image upload process as normal
-            // YOUR IMAGE UPLOAD CODE HERE
-        }
-    }
-    ?>
-    <title>
-        Image Upload
-    </title>
+    <title>Image Upload</title>
     <link rel="stylesheet" href="imup.css">
 </head>
 
 <body>
     <div class='heading'>
-        <button onclick="window.location.href = 'user.php';">
-            &larr; Go Back
-        </button>
+        <button onclick="window.location.href = 'user.php';">&larr; Go Back</button>
         <center>
-            <h1>
-                Image Upload
-            </h1>
+            <h1>Image Upload</h1>
         </center>
         <button onclick="window.location.href = 'logout.php';">Logout</button>
     </div>
+
     <div id="box">
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <table>
                 <tr>
-                    <th>
-                        Image Upload
-                    </th>
-                    <th align="center">
-                        Background
-                    </th>
+                    <th>Image Upload</th>
+                    <th align="center">Background</th>
                 </tr>
                 <tr>
                     <td>
                         <div class="file-upload">
-                            <input type="file" accept="image/png, image/jpeg, image/jpg" name="images[]" multiple required onchange="updateFileName()" id="fileInput" >
+                            <input type="file" accept="image/png, image/jpeg, image/jpg" name="images[]" multiple required onchange="updateFileName()" id="fileInput">
                             <label for="fileInput" class="file-select-button" id="fileName"></label>
                         </div>
-                        <div id="file-selected"></div> <!-- Added div to display selected file names -->
+                        <div id="file-selected"></div>
                     </td>
                     <td>
                         <select name="bg" id="bg">
@@ -63,75 +67,77 @@
                         </select>
                     </td>
                 </tr>
-                <!--******************************************************************************************************* -->
+
                 <?php
                 if ($_SESSION['PORD'] == 'pest' || $_SESSION['PORD'] == 'disease') {
                     echo '<tr>
-                    <th colspan="2">';
+                            <th colspan="2">';
                     $c = "Select " . $_SESSION['CROP'] . " " . $_SESSION['PORD'];
                     echo $c . '-Scientific Name';
                     echo '</th>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                        <select id="Efr" name="Efr">';
+                        </tr>
+                        <tr>
+                            <td colspan="2" align="center">
+                                <select id="Efr" name="Efr">';
                     $c = $_SESSION['CROP'] . $_SESSION['PORD'];
-                    $C = "Select * FROM $c";
-                    if ($result = $conn->query($C)) {
-                        while ($row = $result->fetch_assoc()) {
-                            $PName = $row["PName"];
-                            $SName = $row["SCName"];
-                            $val = $PName . '-' . $SName;
-                            echo '<h1>
-                                <option value=' . urlencode($val) . '>' . $val . '</option>
-                            </h1>';
-                        }
-                        echo '</select>
-                    </td>';
-                    $result->free();
+                    $C = "SELECT * FROM $c";
+                    $result = $conn->query($C);
+                    foreach ($result as $row) {
+                        $PName = $row["PName"];
+                        $SName = $row["SCName"];
+                        $val = $PName . '-' . $SName;
+                        echo '<option value=' . urlencode($val) . '>' . $val . '</option>';
+                    }
+                    echo '</select>
+                            </td>';
+                    $result = null;
                 }
-                }
-                ; ?>
-                <!-- ******************************************************************************************************* -->
+                ?>
+
                 <?php
                 if ($_SESSION['PORD'] == 'pest') {
-                    // Do something if PORD equals 'pest', case-insensitive
-
-                    echo
-                        "<tr>
-                        <th>
-                            What is in the picture?
-                        </th>
-                        <th>
-                            Stage
-                        </th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <select name=" . "Desc" . " id=" . "Desc" . ">
-                                <option value=" . "part" . ">Effected Part</option>
-                                <option value=" . "pest" . ">Pest</option>
-                            </select>
-                        </td>
-                        <td align=" . "center" . ">
-                            <select id=" . "Stage." . " name=" . "Stage" . ">";
-
+                    echo '<tr>
+                            <th>What is in the picture?</th>
+                            <th>Stage</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <select name="Desc" id="Desc">
+                                    <option value="part">Effected Part</option>
+                                    <option value="pest">Pest</option>
+                                </select>
+                            </td>
+                            <td align="center">
+                                <select id="Stage" name="Stage">';
                     $c = 's' . $_SESSION['CROP'] . $_SESSION['PORD'];
-                    $C = "Select * FROM $c";
-                    if ($result = $conn->query($C)) {
-                        while ($row = $result->fetch_assoc()) {
-                            $stage = $row["stages"];
-                            echo '<h1><option value=' . urlencode($stage) . '>' . $stage . '</option></h1>';
-                        }
-                        echo
-                            '</select>
-                        </td>';
-                    $result->free();
+                    $C = "SELECT * FROM $c";
+                    $result = $conn->query($C);
+                    foreach ($result as $row) {
+                        $stage = $row["stages"];
+                        echo '<option value=' . urlencode($stage) . '>' . $stage . '</option>';
+                    }
+                    echo '</select>
+                            </td>
+                        </tr>';
+                    $result = null;
+                } elseif ($_SESSION['PORD'] == 'Nutrient Defficiency') {
+                    $stmt = $conn->query("SELECT * FROM nutrients");
+                    $data = $stmt->fetchAll();
+                    echo '<tr>
+                            <th colspan="2">Nutrient Deficiency</th>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <select>';
+                    foreach ($data as $row) {
+                        $o = $row['name'];
+                        echo '<option value=' . $o . '>' . $o . '</option> ';
+                    }
+                    echo '</select>
+                            </td>
+                        </tr>';
                 }
-                echo "
-                        </td>
-                    </tr>";
-                } ?>
+                ?>
             </table>
             <input type="submit" value="Upload Images">
         </form>
