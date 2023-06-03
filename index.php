73ab484ebef99d381e52907e0eaab04a63c9f63c
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2023-06-03 19:50:06 +0530

    Mobile View enhanced

--- index.php
+++ index.php
@@ -1,42 +1,35 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Image Repository</title>
+  <link rel="stylesheet" href="index.css">
+</head>
 <body>
-<link rel="stylesheet" href="index.css">
-<?php session_start();
-	if(isset($_SESSION['UN'])){
-		header("location:logout.php");
-		exit;
-	}
-?>
-<div class='heading'>
-  <img src="icar.png" alt=""height=5% width=5%>
-  <center>
-  <h1 class='hh'>Oil Seeds Pests and Diseases Image Repository System V<span class='v'>2.0</span></h1>
-  </center>
-  <img src="iior-logo.jpg" alt=""height=5% width=5%>
+<div class="heading">
+  <img src="icar.png" alt="">
+  <h1>Oil Seeds Pests and Diseases Image Repository System V2.0</h1>
+  <img src="iior-logo.jpg" alt="">
 </div>
 <div class="log">
-<div class="login">
-<h2>LOGIN</h2>
-<form action="./validation.php" method="POST">
-  
-  <label for="User"><h3>User Name:</h3></label>
-  <input type="text" id="UserName" name="UserName" value="" placeholder="Enter your User Name" required>
-  
-  <label for="password"><h3>Password:</h3></label>
-  <input type="password" id="password" name="password" value="" placeholder="Enter your Password" required>
-  <div class="select-container">
-  <label for="role"><h3>Role:</h3></label>
-  <select id="role" name="role">
-        <option value="User">User</option>
-        <option value="Moderator">Approver</option>
-        <option value="Admin">Admin</option>
-  </select>
-  <span class="select-arrow">&#9662;</span>
-</div>
-<br>
-  <input type="submit" value="Submit">
-</form> 
+  <div class="login">
+    <center><h2>LOGIN</h2></center>
+    <form action="./validation.php" method="POST">
+      <label for="UserName">User Name:</label>
+      <input type="text" id="UserName" name="UserName" placeholder="Enter your User Name" required>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" placeholder="Enter your Password" required>
+      <div class="select-container">
+        <label for="role">Role:</label>
+        <select id="role" name="role">
+          <option value="User">User</option>
+          <option value="Moderator">Approver</option>
+          <option value="Admin">Admin</option>
+        </select>
+        <span class="select-arrow">&#9662;</span>
+      </div>
+      <input type="submit" value="Submit">
+    </form>
   </div>
 </div>
 </body>
