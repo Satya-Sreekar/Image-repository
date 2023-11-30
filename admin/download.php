7ef2b4d1bd16dbf0f69002a83b632df3f1350c9b
Sreekar <satyasreekarpattaswami@gmail.com>
2023-12-01 01:57:56 +0530

    fixes the issue of nested folders

--- admin/download.php
+++ admin/download.php
@@ -4,12 +4,11 @@ require('../DBinfo.php');
 try {
     // Create a new PDO instance
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Query to retrieve all images from the table
-    $sql = "SELECT `IName`, `IMAGE` FROM permdb;";
+    $sql = "SELECT `IName`,`IMAGE` FROM permdb;";
 
     // Prepare the SQL query
     $stmt = $conn->prepare($sql);
@@ -27,42 +26,39 @@ try {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $image_data = $row['IMAGE'];
         $iname = $row['IName']; // Get the IName column value
-
         // Generate a unique filename for each image
         $filename = $iname . '.jpg'; // Rename the file using IName
 
         // Save the image to the folder
-        file_put_contents($folder_name . '/' . $filename, $image_data);
+        if (file_put_contents($folder_name . '/' . $filename, $image_data) === false) {
+            // Handle error (e.g., log it or inform the user)
+        }
     }
 
-    // Create a zip file containing the downloaded images
+    // Create a zip archive containing the images
+    $zipFilename = $folder_name . '.zip';
     $zip = new ZipArchive();
-    $zipFileName = $folder_name . '.zip';
-    if ($zip->open($zipFileName, ZipArchive::CREATE) === true) {
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder_name));
-        foreach ($files as $file) {
-            if (!$file->isDir()) {
-                $filePath = $file->getRealPath();
-                $relativePath = substr($filePath, strlen($folder_name) + 1);
-                $zip->addFile($filePath, $relativePath);
-            }
+    if ($zip->open($zipFilename, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
+        foreach (glob($folder_name . '/*') as $file) {
+            $zip->addFile($file, basename($file));
         }
         $zip->close();
+
+        // Send the zip file to the user
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename=' . $zipFilename);
+        readfile($zipFilename);
+
+        // Delete the zip file after sending
+        unlink($zipFilename);
+    } else {
+        // Handle zip creation error
     }
-
-    // Set headers to force download the zip file
-    header('Content-Type: application/zip');
-    header('Content-Disposition: attachment; filename=' . basename($zipFileName));
-    header('Content-Length: ' . filesize($zipFileName));
-
-    // Send the zip file to the browser
-    readfile($zipFileName);
-
-    // Remove the temporary files and folder
-    unlink($zipFileName);
-    $conn = null;
-    rmdir($folder_name);
 } catch (PDOException $e) {
+    // Handle database connection or query error
     echo "Error: " . $e->getMessage();
 }
+
+// Close the database connection
+$conn = null;
 ?>
